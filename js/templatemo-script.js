c1741a05008f0476f4a80897f8c70d2353d4bcb1
Arun G Nayak <nayakarun02@gmail.com>
2022-10-02 20:42:21 +0530

    Initial commit

---
+++ js/templatemo-script.js
@@ -0,0 +1,16 @@
+/**
+ *	www.templatemo.com
+ */
+
+/* HTML document is loaded. DOM is ready.
+-----------------------------------------*/
+$(document).ready(function(){
+
+	// Mobile menu
+	$('.mobile-menu-icon').click(function(){
+		$('.tm-nav').toggleClass('show');
+	});
+  
+  	// http://stackoverflow.com/questions/2851663/how-do-i-simulate-a-hover-with-a-touch-in-touch-enabled-browsers
+  	$('body').bind('touchstart', function() {});
+});
