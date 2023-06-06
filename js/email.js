4d6c35fe95d9851815ccf75fa14dcc703c3629bd
MD SHAHIL <96812916+Shahil093@users.noreply.github.com>
2023-06-06 10:00:16 +0530

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,12 +1,15 @@
+const sendEmail = () => {
+  const name = document.getElementById('contact_name').value;
+  const email = document.getElementById('contact_email').value;
+  const subject = document.getElementById('contact_subject').value;
+  let body = document.getElementById('contact_message').value;
+  body = "Name - " + name + " | Email - " + email + " | Message - " + body;
 
-const sendEmail = ()=>{
-    const name = document.getElementById('contact_name').value;
-    const email = document.getElementById('contact_email').value;
-    const subject = document.getElementById('contact_subject').value;
-    let body = document.getElementById('contact_message').value;
-    body = "Name - "+ name +" | email - "+email+" | Message - "+body ;
-    
-    console.log(name, email , subject , body);
-    window.open('mailto:your@email.address?subject='+subject+'&body='+body);
-}
+  console.log(name, email, subject, body);
 
+  const mailtoLink = "mailto:your@email.address"
+    + "?subject=" + encodeURIComponent(subject)
+    + "&body=" + encodeURIComponent(body);
+
+  window.open(mailtoLink);
+};
